6753f84983729808fe506f2e783d0165cb5c9366
nikkivasilev <n.vasilev105@gmail.com>
2023-02-04 12:40:39 +0200

    implemented mediafiles

--- online_library/settings.py
+++ online_library/settings.py
@@ -99,7 +99,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static/',
 )
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+        BASE_DIR / 'mediafiles/'
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.AppUser'
--- online_library/urls.py
+++ online_library/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,3 +9,7 @@ urlpatterns = [
     path('accounts/', include('online_library.accounts.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
