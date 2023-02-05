4387da2ba1a39b487c4d19238405336c458a6761
nikkivasilev <n.vasilev105@gmail.com>
2023-02-05 14:42:37 +0200

    implemented book model

--- online_library/library/admin.py
+++ online_library/library/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from online_library.library.models import Book
+
+
 # Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type')
+    search_fields = ('title',)
--- online_library/library/models.py
+++ online_library/library/models.py
@@ -1,9 +1,43 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-#TODO: implement books
+from online_library.accounts.validators import validate_file_size
 
-# class Book(models.Model):
-#     title = models.CharField(max_length=30)
-#     description = models.TextField()
-#     image = models.URLField()
-#     type = models.CharField(max_length=30)
+UserModel = get_user_model()
+
+class Book(models.Model):
+    TITLE_MAX_LENGTH = 75
+    TYPE_MAX_LENGTH = 75
+    DESCRIPTION_MAX_LENGTH = 355
+
+    title = models.CharField(
+        max_length=TITLE_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    description = models.TextField(
+        max_length=DESCRIPTION_MAX_LENGTH,
+        null=True,
+        blank=True,
+    )
+
+    image = models.URLField(
+        null=False,
+        blank=False,
+    )
+    type = models.CharField(
+        max_length=TYPE_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f'{self.title} - {self.type}'
